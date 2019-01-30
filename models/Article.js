8e2c542ec635fa9a4e68b2a3aa015caf8defefb2
Zee <zzeinabfaragg@gmail.com>
2019-01-30 00:34:58 -0500

    update

--- models/Article.js
+++ models/Article.js
@@ -1,54 +1,42 @@
-//Mongoose
-//------------------------------------
 var mongoose = require("mongoose");
-
-//db
-//------------------------------------
 var Schema = mongoose.Schema;
 
-//db articles obj
-//------------------------------------
-
-//Title, link, summary, img, saved boolean, status boolean, date & note
 var Articleschema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
-  summary: {
-    type: String,
-    default: "unavailable summary."
-  },
-  img: {
-    type: String,
-    default: "unavailable image."
-  },
-  issaved: {
-    type: Boolean,
-    default: false
-  },
-  status: {
-    type: String,
-    default: "Save Article"
-  },
-  created: {
-    type: Date,
-    default: Date.now
-  },
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+	title: {
+		type: String,
+		required: true,
+	},
+	link: {
+		type: String,
+		required: true,
+	},
+	summary: {
+		type: String,
+		default: "Summary unavailable."
+	},
+	img: {
+		type: String,
+		default: "/assets/images/unavailable.jpg"
+	},
+	issaved: {
+		type: Boolean,
+		default: false
+	},
+	status: {
+		type: String,
+		default: "Save Article"
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	note: {
+		type: Schema.Types.ObjectId,
+		ref: "Note"
+	}
 });
 
-Articleschema.index({ title: "text" });
+Articleschema.index({title: "text"});
 
-var Article = mongoose.model("Article", Articleschema);
-
-//Export
-//------------------------------------
+var Article  = mongoose.model("Article", Articleschema);
 module.exports = Article;
